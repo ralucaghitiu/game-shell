c39b48f0bcb3a00b33f75758866092e08ee210ff
ralucaghitiu <b.ralucaelena@gmail.com>
2022-11-07 22:27:43 +0200

    curs 07.11 Redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Button } from '../components/common/ui';
 import { Layout } from '../layouts';
+import { add, sub, addNumber } from '../store/actions';
+import { setUser } from '../store/actions/authActions';
 
 export const Home = () => {
+  const dispatch = useDispatch();
+  const count = useSelector(({ counter }) => {
+    return counter;
+  });
+
   return (
     <Layout>
       <div className="container mx-auto px-4">
@@ -10,6 +18,48 @@ export const Home = () => {
         <Link to="/play" title="Play a game">
           <Button element="span">Play</Button>
         </Link>
+
+        <div className="mt-11">
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(sub());
+            }}
+          >
+            -
+          </button>
+          <span>{count}</span>
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(add());
+            }}
+          >
+            +
+          </button>
+        </div>
+
+        <div className="mt-11">
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(addNumber(5));
+            }}
+          >
+            Add 5
+          </button>
+        </div>
+
+        <div className="mt-11">
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(setUser());
+            }}
+          >
+            Set Carol
+          </button>
+        </div>
       </div>
     </Layout>
   );
--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,7 +1,11 @@
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Layout } from '../layouts';
+import { add } from '../store/actions';
 
 export const Play = () => {
+  const dispatch = useDispatch();
+
   return (
     <Layout>
       <div className="mx-auto container px-4">
@@ -10,6 +14,15 @@ export const Play = () => {
         <Link to="/" title="Home">
           Home
         </Link>
+
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(add());
+          }}
+        >
+          Add
+        </button>
       </div>
     </Layout>
   );
